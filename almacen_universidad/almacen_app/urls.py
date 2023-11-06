6e6d786c1a0e2b2228cbb72a5312e62cef0dcc4b
VRFT <112048785+VRFT@users.noreply.github.com>
2023-11-05 21:46:09 -0600

    Update urls.py
    URL de registro de personas

--- almacen_universidad/almacen_app/urls.py
+++ almacen_universidad/almacen_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
    path('', views.login_view, name='login'),  # Cambia la ruta de '' a 'login/'
-    path('welcome/', views.welcome_view, name='welcome'),
-    # ... otras rutas ...
+   path('welcome/', views.welcome_view, name='welcome'),
+   # La siguiente linea ayudara a redirigir al usuario al apartado de registrar persona
+   path('registrar_persona_prototipo/', views.registrar_persona_prototipo, name='registrar_persona_prototipo'),
 ]
