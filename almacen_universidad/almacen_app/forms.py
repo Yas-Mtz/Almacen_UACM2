0be953875ee8cbcba4b70adc4970862cac445f14
VRFT <112048785+VRFT@users.noreply.github.com>
2023-11-06 23:43:05 -0600

    Add files via upload
    Esto ayudara a realizar el formulario que llenaremos para el registro de una persona

---
+++ almacen_universidad/almacen_app/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class RegistroPersonaForm(forms.Form):
+    nombre = forms.CharField(max_length=255)
+    apellido_paterno = forms.CharField(max_length=255)
+    apellido_materno = forms.CharField(max_length=255)
+    telefono = forms.CharField(max_length=15)
+    correo = forms.EmailField()
+    contrasena = forms.CharField(max_length=255, widget=forms.PasswordInput())
