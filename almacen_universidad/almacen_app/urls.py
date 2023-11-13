18d4a0698025574d1313e1f2ed6167fa89895976
VRFT <112048785+VRFT@users.noreply.github.com>
2023-11-12 21:51:16 -0600

    Update urls.py
    Comentario para las url´s

--- almacen_universidad/almacen_app/urls.py
+++ almacen_universidad/almacen_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.login_view, name='login'),  # Cambia la ruta de '' a 'login/'
+   path('', views.login_view, name='login'),  ## Redirige la raíz a la vista de inicio de sesión
    path('welcome/', views.welcome_view, name='welcome'),
    # La siguiente linea ayudara a redirigir al usuario al apartado de registrar persona
    path('registrar_persona_prototipo/', views.registrar_persona_prototipo, name='registrar_persona_prototipo'),
